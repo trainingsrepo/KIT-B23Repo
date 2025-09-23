1b6f6e7d48a880921d4563161cffbdc95b01fbc6
trainingsrepo <trainings.anil@gmail.com>
2025-09-23 14:30:17 +0530

    feature101 changes in Application.java file

--- Application.java
+++ Application.java
@@ -60,6 +60,24 @@ public class Bank {
     }
     checkBalance();
   }
+  #code changes for feature101 by Vivek in oct 2024 for Reports Module
+  public class SimpleMethodExample {
+
+    // Method that takes two integers and returns their sum
+    public static int addNumbers(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        // Calling the method and storing the result
+        int result = addNumbers(5, 7);
+
+        // Printing the result
+        System.out.println("The sum is: " + result);
+    }
+}
+#end of feature101
 
   public static void exit() {
     System.out.println("Thank you for banking with us. Have a great day!");
