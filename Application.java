792aa04b03775d8343de7450930845c128b0148e
trainingsrepo <trainings.anil@gmail.com>
2025-09-23 14:34:14 +0530

    feature102 changes

--- Application.java
+++ Application.java
@@ -60,7 +60,31 @@ public class Bank {
     }
     checkBalance();
   }
+#Code changes for feature102 by ethan in Accounts Module in Oct 2024
+// A class with public access specifier
+public class MyClass {
 
+    // Public field
+    public int publicField = 10;
+
+    // Public method
+    public void publicMethod() {
+        System.out.println("This is a public method.");
+    }
+
+    // Main method to run the program
+    public static void main(String[] args) {
+        // Creating an object of MyClass
+        MyClass myObject = new MyClass();
+
+        // Accessing the public field
+        System.out.println("Public Field: " + myObject.publicField);
+
+        // Calling the public method
+        myObject.publicMethod();
+    }
+}
+#end of feature102
   public static void exit() {
     System.out.println("Thank you for banking with us. Have a great day!");
   }
